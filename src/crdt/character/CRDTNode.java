bc3cab7cad6024d907e1a561e021ccfeef4ee00b
MohamedShalaby <Mohamed.Shalaby04@eng-st.cu.edu.eg>
2026-04-01 01:19:43 +0200

    CRDT Node Created

--- src/crdt/character/CRDTNode.java
+++ src/crdt/character/CRDTNode.java
@@ -1,4 +1,22 @@
 package crdt.character;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CRDTNode {
+    String id;
+    char value;
+    boolean del;
+    CRDTNode parent;
+    List<CRDTNode> children;
+
+    public CRDTNode(String id, char value, CRDTNode parent) {
+        this.id = id;
+        this.value = value;
+        this.parent = parent;
+        this.del = false; //default
+        this.children = new ArrayList<>();
+    }
+
+
 }
