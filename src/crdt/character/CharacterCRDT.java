53cdd9b1d0d5093b98a353ec2de5b9612c014949
MohamedShalaby <Mohamed.Shalaby04@eng-st.cu.edu.eg>
2026-04-01 18:36:13 +0200

    Updated CRDT Character handling, ID + Clock

--- src/crdt/character/CharacterCRDT.java
+++ src/crdt/character/CharacterCRDT.java
@@ -1,6 +1,7 @@
 package crdt.character;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,31 +9,39 @@ import java.util.Map;
 public class CharacterCRDT {
 
     public Map<String, CRDTNode> nodeMap;
-    public List<CRDTNode> roots; //nodes with parent = null
+    public List<CRDTNode> roots; // nodes with parent = null
 
     public CharacterCRDT() {
         nodeMap = new HashMap<>();
         roots = new ArrayList<>();
     }
 
-    public void insert(String id, char value, String parentId) {
+    public void insert(int userId, int clock, char value, String parentId) {
         CRDTNode parent = null;
+
         if (parentId != null) {
             parent = nodeMap.get(parentId);
+
+            if (parent == null) {
+                System.out.println("Insert failed: parent with id " + parentId + " not found.");
+                return;
+            }
         }
-        CRDTNode newNode = new CRDTNode(id, value, parent);
+
+        CRDTNode newNode = new CRDTNode(userId, clock, value, parent);
 
         if (parent == null) {
             roots.add(newNode);
+            sortNodes(roots);
         } else {
             parent.children.add(newNode);
+            sortNodes(parent.children);
         }
 
-        nodeMap.put(id, newNode);
+        nodeMap.put(newNode.getId(), newNode);
     }
 
     public void delete(String id) {
-
         CRDTNode node = nodeMap.get(id);
 
         if (node != null) {
@@ -60,4 +69,16 @@ public class CharacterCRDT {
         }
     }
 
+    private void sortNodes(List<CRDTNode> nodes) {
+        nodes.sort(new Comparator<CRDTNode>() {
+            @Override
+            public int compare(CRDTNode a, CRDTNode b) {
+
+                if (a.clock != b.clock) {
+                    return Integer.compare(b.clock, a.clock);
+                }
+                return Integer.compare(a.userId, b.userId);
+            }
+        });
+    }
 }
